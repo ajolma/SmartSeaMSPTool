368fafbc74e3c6ac8a903f086053db2642c94633
Ari Jolma <ari.jolma@gmail.com>
2017-01-05 15:19:57 +0200

    Clean Explain from old crud. Clean up index.html, add a specific legend for value layers.

--- SmartSea/Explain.pm
+++ SmartSea/Explain.pm
@@ -16,9 +16,9 @@ binmode STDERR, ":utf8";
 sub new {
     my ($class, $self) = @_;
     $self = Plack::Component->new($self);
-    $self->{sea} = Geo::GDAL::Open("$self->{config}{data_path}/sum.tiff");
-    $self->{depth} = Geo::GDAL::Open("$self->{config}{data_path}/depth-classes.tiff");
-    $self->{natura} = Geo::GDAL::Open("$self->{config}{data_path}/natura.tiff");
+    my $dsn = "dbi:Pg:dbname=$self->{dbname}";
+    $self->{schema} = SmartSea::Schema->connect($dsn, $self->{user}, $self->{pass}, {});
+    $self->{mask} = Geo::GDAL::Open("$self->{data_path}/smartsea-mask.tiff");
     return bless $self, $class;
 }
 
@@ -30,54 +30,60 @@ sub call {
     my $parameters = $request->parameters;
     for my $key (sort keys %$parameters) {
         my $val = $parameters->{$key} // '';
-        say STDERR "$key => $val";
+        #say STDERR "$key => $val";
     }
-    my @layers = $request->query_parameters->get_all('layer');
-    say STDERR "layers = @layers";
-    my @layer_names = @layers;
-    for (@layer_names) {
-        s/_/ /g;
-        s/^(\w)/uc($1)/e;
-        s/ (\w+)$/, $1<br \/>&nbsp;&nbsp;bla bla<br \/>/;
+    my @rules;
+    for my $layer ($request->query_parameters->get_all('layer')) {
+        push @rules, SmartSea::Rules->new($self->{schema}, $layer);
     }
 
-    my $gt = $self->{sea}->GeoTransform;
-    my $report = "@layer_names<br />";
+    my $gt = $self->{mask}->GeoTransform;
+    my $report = '';
 
-    if (@layers == 0) {
+    if (@rules == 0) {
 
         $report = 'No selected layers';
 
     } elsif ($parameters->{wkt}) {
-        my ($canvas, $extent, $overview, $cell_area, @clip) = canvas($gt, $parameters->{wkt});
+
+        my $e = $self->{mask}->Extent;
+        my @points = ([$e->[0],$e->[1]], [$e->[0],$e->[3]], [$e->[2],$e->[3]], [$e->[2],$e->[1]]);
+        push @points, $points[0];
+        my $region = Geo::OGR::Geometry->new(GeometryType => 'Polygon', Points => [[@points]]);
+        my ($canvas, $extent, $overview, $cell_area, @clip) = canvas($gt, $parameters->{wkt}, $region);
         
         my $a = $canvas->Band()->Piddle();
-        my $A = sum($a);
-        say STDERR "cells in polygon $A";
+        my $s = $self->{mask}->Band()->Piddle(@clip);
+        my $A = sum($a*$s); # cells in polygon
 
-        $report .= '(from overview) ' if $overview;
-        my $s;
-        eval {
-            $s = $self->{depth}->Band()->Piddle(@clip);
-        };
-        unless ($@) {
-            $s = $a*($s+1); # adjust land cover values to 1..4
-            my @lc;
-            for my $i (0..3) {
-                my $result = $a*0;
-                my $x = $result->where($s == ($i+1));
-                $x .= 1;
-                $lc[$i] = int(sum($result)/$A*100);
+        $report .= 'Following are estimates.<br />' if $overview;
+        $report .= 'The polygon is clipped to the sea area.<br />';
+        $report .= "Size of the selected area: " . int($A*$cell_area+0.5) . " km2";
+        
+        # the idea here would be to compute average values and/or amount of allocated areas
+        # in the polygon area
+        if (0) {
+            my $s;
+            eval {
+                $s = $self->{mask}->Band()->Piddle(@clip);
+            };
+            unless ($@) {
+                $s = $a*($s+1); # adjust land cover values to 1..4
+                my @lc;
+                for my $i (0..3) {
+                    my $result = $a*0;
+                    my $x = $result->where($s == ($i+1));
+                    $x .= 1;
+                    $lc[$i] = int(sum($result)/$A*100);
+                }
+                $report .= "land " . $lc[0] . '%' .
+                    ', shallow ' . $lc[1] . '%' .
+                    ', trans ' . $lc[2] . '%' .
+                    ', deep ' . $lc[3] . '%';
+            } else {
+                $report .= 'no data';
             }
-            $report .= "land " . $lc[0] . '%' .
-                ', shallow ' . $lc[1] . '%' .
-                ', trans ' . $lc[2] . '%' .
-                ', deep ' . $lc[3] . '%';
-        } else {
-            $report .= 'no data';
         }
-        $report .= ", total " . int($A*$cell_area+0.5) . " km2";
-        say STDERR $report;
 
     } else {
 
@@ -87,15 +93,12 @@ sub call {
         my $d = 0;
         my $n = 0;
         eval {
-            $d = $self->{depth}->Band(1)->ReadTile($x, $y, 1, 1)->[0][0];
-            $n = $self->{natura}->Band(1)->ReadTile($x, $y, 1, 1)->[0][0];
+            $d = $self->{mask}->Band(1)->ReadTile($x, $y, 1, 1)->[0][0];
         };
-        say STDERR "depth = $d";
-        say STDERR "natura = $n";
 
-        my %d = (1 => 'Shallow', 2 => 'Transitional', 3 => 'Deep');
+        my %d = (0 => 'Outside of region', 1 => 'Inside of region');
 
-        $report .= decode(utf8 => $d{$d} // 'No data') . ' ' . $n;
+        $report .= $d{$d};
 
     }
 
@@ -104,16 +107,17 @@ sub call {
 }
 
 sub canvas {
-    my ($gt, $wkt) = @_;
+    my ($gt, $wkt, $region) = @_;
     my $g = Geo::OGR::Geometry->new(WKT => $wkt);
+    $g = $g->Intersection($region);
     my $e = $g->Extent;
     my $inv = $gt->Inv;
     my ($l_col, $u_row) = $inv->Apply($e->[0], $e->[3]);
     $l_col = int($l_col);
     $u_row = int($u_row);
     my ($r_col, $d_row) = $inv->Apply($e->[1], $e->[2]);
-    $r_col = int($r_col)+1;
-    $d_row = int($d_row)+1;
+    $r_col = int($r_col);
+    $d_row = int($d_row);
     my $w = $r_col - $l_col;
     my $h = $d_row - $u_row;
     
@@ -137,7 +141,6 @@ sub canvas {
     my $gty = $gt->[5]*($h/$H); # assuming north up
     $canvas->GeoTransform($ulx, $gtx, 0, $uly, 0, $gty);
     my $cell_area = abs($gtx * $gty)/1000000.0; # km2
-    say STDERR "cell area $cell_area";
 
     # create a layer, which to draw on the canvas
     my $wkt_ds = Geo::OGR::Driver('Memory')->Create('wkt');
