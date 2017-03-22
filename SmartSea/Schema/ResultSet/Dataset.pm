7b53978414fdddfe1a416fcf894ca79262d07252
Ari Jolma <ari.jolma@gmail.com>
2017-03-22 20:57:13 +0200

    Add Dataset.ResultSet.

---
+++ SmartSea/Schema/ResultSet/Dataset.pm
@@ -0,0 +1,23 @@
+package SmartSea::Schema::ResultSet::Dataset;
+
+use strict; 
+use warnings;
+
+use base 'DBIx::Class::ResultSet';
+
+sub list {
+    my $self = shift;
+    $self->search({ -and => [is_a_part_of => undef, is_derived_from => undef] });
+}
+
+sub parts {
+    my ($self, $of) = @_;
+    $self->search({is_a_part_of => $of->id});
+}
+
+sub derivatives {
+    my ($self, $of) = @_;
+    $self->search({is_derived_from => $of->id});
+}
+
+1;
